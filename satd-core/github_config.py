bb21ea490bad49f96a4f79f9b376e8933fef8937
Shahidul Islam <shahidul2k9@gmail.com>
2025-02-11 10:52:21 -0600

    update env cofig

--- satd-core/github_config.py
+++ satd-core/github_config.py
@@ -1,8 +1,17 @@
 # GitHub API URL (Sorted by stars, limited to 100 results per page)
+import os
+
+from dotenv import load_dotenv
+
 BASE_URL = "https://api.github.com/search/repositories"
+
+load_dotenv()
+# GitHub API URL (Sorted by stars, limited to 100 results per page)
+BASE_URL = "https://api.github.com/search/repositories"
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
 # Headers with authentication (replace with your personal GitHub token for higher limits)
 HEADERS = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization": "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"  # Optional, to avoid rate limits
+    "Authorization": f'token {GITHUB_TOKEN}'
 }
--- sql/v1-init.sql
+++ sql/v1-init.sql
@@ -26,3 +26,47 @@ CREATE TABLE repository
     updated_at            TIMESTAMP
 
 );
+
+DROP TABLE IF EXISTS comment;
+CREATE TABLE comment
+(
+    id                    BIGSERIAL PRIMARY KEY,
+    repository_id         BIGINT UNIQUE NOT NULL,               -- Unique GitHub Repository ID
+    name                  VARCHAR(255)  NOT NULL,               -- Repository Name
+    full_name             VARCHAR(255)  NOT NULL,               -- Full name (owner/repo)
+    is_fork               BOOLEAN       NOT NULL DEFAULT FALSE, -- Whether the repo is a fork or not
+    owner                 VARCHAR(255)  NOT NULL,               -- Repository Owner
+    owner_url             TEXT,                                 -- Owner GitHub Profile URL
+    repo_url              TEXT          NOT NULL,               -- Repository GitHub URL
+    stars                 INT           NOT NULL,               -- Number of Stars
+    forks                 INT           NOT NULL,               -- Number of Forks
+    watchers              INT           NOT NULL,               -- Number of Watchers
+    language              VARCHAR(50),                          -- Main Language of Repo
+    description           TEXT,                                 -- Repository Description
+    open_issues           INT                    DEFAULT 0,     -- Open Issues Count
+    license_name          VARCHAR(255),                         -- License Type
+    topics                TEXT[],                               -- Array of Topics/Tags
+    default_branch        VARCHAR(50),                          -- Default Branch (main/master)
+    commit_hash           VARCHAR(255),                         -- Last Commit Hash
+    pushed_at             TIMESTAMP,                            -- Last Commit Push Timestamp
+    repository_created_at TIMESTAMP,                            -- Repository Creation Date
+    repository_updated_at TIMESTAMP,                            -- Last Update Timestamp
+    created_at            TIMESTAMP              default now(),
+    updated_at            TIMESTAMP
+
+);
+
+project name
+commit_hash
+file
+file hash
+comment
+start line
+end line
+comment hash
+is satd
+satd type
+pattern type
+target element
+note
+language
