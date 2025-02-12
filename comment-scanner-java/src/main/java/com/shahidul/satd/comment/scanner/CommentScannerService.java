abf8e0a25223e505b65149aa902a02bda4e38911
Shahidul <shahidul2k9@gmail.com>
2025-02-12 17:39:34 -0600

    update comment parsing logic

--- comment-scanner-java/src/main/java/com/shahidul/satd/comment/scanner/CommentScannerService.java
+++ comment-scanner-java/src/main/java/com/shahidul/satd/comment/scanner/CommentScannerService.java
@@ -1,18 +1,6 @@
 package com.shahidul.satd.comment.scanner;
 
-import java.util.Set;
-
 public interface CommentScannerService {
-    Set<String> TEST_FRAMEWORK_ANNOTATIONS = Set.of(
-            // JUnit 4
-            "Test", "Before", "After", "BeforeClass", "AfterClass",
-            // JUnit 5
-            "BeforeEach", "AfterEach", "BeforeAll", "AfterAll", "DisplayName",
-            // Mockito
-            "Mock", "InjectMocks", "Spy", "Captor",
-            // TestNG
-            /*"Test",*/ "BeforeMethod", "AfterMethod"/*, "BeforeClass", "AfterClass"*/
-    );
 
     int scanComment(String directory, Boolean isTestCode, String outputFile);
 }
