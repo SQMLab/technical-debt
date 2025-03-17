bf0dfa285409c639bb5e7216b30fe73c91514ffc
Shahidul Islam <shahidul2k9@gmail.com>
2025-03-17 00:35:06 -0500

    update comment csv

--- sql/util.sql
+++ sql/util.sql
@@ -1,5 +1,12 @@
-SELECT id, text,
-       CASE WHEN is_td = TRUE THEN 'yes' ELSE 'no' END AS label
-FROM comment
-WHERE is_td = TRUE
-  AND repository_id != 69 order by id;
+SELECT id,
+       repository_id                                   as repository,
+       text                                            as comment,
+       CASE WHEN is_td = TRUE THEN 'yes' ELSE 'no' END AS satd,
+       td_type                                         as type
+from comment
+where is_random = TRUE order by id;
+
+select td_type, count(id)
+from comment
+where td_type is not null
+group by td_type;
