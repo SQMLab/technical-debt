725e9294c7548888546939057e4607f1b29b5745
Shahidul Islam <shahidul2k9@gmail.com>
2025-03-17 01:18:51 -0500

    remove 506 comments containing non ASCII text as substring

--- sql/util.sql
+++ sql/util.sql
@@ -1,3 +1,4 @@
+-- Export Dataset
 SELECT id,
        repository_id                                   as repository,
        text                                            as comment,
@@ -6,7 +7,19 @@ SELECT id,
 from comment
 where is_random = TRUE order by id;
 
+-- Classified SATD Count
 select td_type, count(id)
 from comment
 where td_type is not null
 group by td_type;
+
+-- Chinese Text Filter
+select *
+FROM comment
+WHERE text ~* '[\u4e00-\u9fff]' and is_random = TRUE;
+
+--  Matches non-Latin characters
+SELECT *
+FROM comment
+WHERE NOT (text ~* '^[\x00-\x7F]*$')
+AND is_random = TRUE;
