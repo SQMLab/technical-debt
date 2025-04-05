e05ea8882e7e226bc3817752dd70e2c5d8f33d92
Shahidul <shahidul2k9@gmail.com>
2025-04-04 19:41:22 -0500

    add repository list file

--- sql/util.sql
+++ sql/util.sql
@@ -23,3 +23,61 @@ SELECT *
 FROM comment
 WHERE NOT (text ~* '^[\x00-\x7F]*$')
 AND is_random = TRUE;
+
+
+-- 10 Well-Known Projects
+SELECT
+    r.name,
+    r.stars,
+    r.forks,
+    r.watchers,
+    COUNT(c.id) AS comments
+FROM
+    repository r
+INNER JOIN
+    comment c ON r.id = c.repository_id
+WHERE
+    r.id IN (6, 8, 11, 13, 15, 17, 21, 28, 32, 40, 51, 53, 59, 60, 61, 63, 67, 69, 74, 81, 83, 86, 144, 173, 182)
+GROUP BY
+    r.id
+LIMIT 10;
+
+-- Top 10 project with high number of comments
+SELECT
+    r.name,
+    r.stars,
+    r.forks,
+    r.watchers,
+    COUNT(c.id) AS comments
+FROM
+    repository r
+INNER JOIN
+    comment c ON r.id = c.repository_id
+GROUP BY
+    r.id
+order by COUNT(c.id) desc
+LIMIT 10;
+
+-- TOP 10 SATD project
+SELECT
+    r.name,
+    COUNT(c.id) AS comments,
+    COUNT(CASE WHEN c.is_random = true THEN 1 END) AS reviewed_comments,
+    COUNT(CASE WHEN c.is_random = true AND c.is_td = true THEN 1 END) AS satd_comments,
+    CONCAT(
+        COUNT(CASE WHEN c.is_random = true AND c.is_td = true THEN 1 END),
+        ' / ',
+        COUNT(CASE WHEN c.is_random = true THEN 1 END)
+    ) AS satd_comment_by_reviewed_comment,
+    ROUND(
+        100.0 * COUNT(CASE WHEN c.is_random = true AND c.is_td = true THEN 1 END)
+        / NULLIF(COUNT(CASE WHEN c.is_random = true THEN 1 END), 0), 2
+    ) AS satd_percentage
+FROM
+    repository r
+INNER JOIN
+    comment c ON r.id = c.repository_id
+GROUP BY
+    r.id
+ORDER BY
+    COUNT(CASE WHEN c.is_random = true THEN 1 END) DESC;
