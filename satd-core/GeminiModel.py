065c91c48b9d34c8604169378144c01a1e582ac6
Shahidul Islam <shahidul2k9@gmail.com>
2025-09-15 16:57:18 -0500

    add caching configuration

--- satd-core/GeminiModel.py
+++ satd-core/GeminiModel.py
@@ -23,8 +23,8 @@ def predict_with_gemini(client, model_name, system_instruction, prompt):
 
 
 class GeminiModel(Model):
-    def __init__(self, task_type: str, model_uri: str,  output_label_converter: OutputLabelConverter):
-        super().__init__(task_type, model_uri, output_label_converter)
+    def __init__(self, task_type: str, model_uri: str,  output_label_converter: OutputLabelConverter, enable_cache: bool):
+        super().__init__(task_type, model_uri, output_label_converter, enable_cache)
         self.model =  genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
         self.train_dataset = None
         self.train_indexes = None
@@ -33,22 +33,21 @@ class GeminiModel(Model):
         self.train_dataset = dataset
 
     def predict(self, dataset: Dataset, dataset_name: str, prompt_template: PromptTemplate, train_strategy: TrainStrategy, n_shot_size: int, verbose: bool = False):
-        super().predict_start(dataset)
+        model_name_suffix  = f'{n_shot_size}-shot'
+        super().predict_start(dataset, model_name_suffix)
         label_predictions = []
         raw_label_predictions = []
-        model_name_suffix  = f'{n_shot_size}-shot'
         for index in range(dataset.num_rows):
             train_indexes = pick_n_shot(self.train_dataset, dataset, index, n_shot_size, train_strategy)
             prompt = self.create_prompt(prompt_template, self.train_dataset, train_indexes, dataset, index, verbose)
-            cached_output_label = self.read_from_merged_file(model_name_suffix, dataset['text'][index])
-            if cached_output_label is None:
-                raw_label = predict_with_gemini(self.model, self.model_uri,f'{prompt_template.definition} {prompt_template.instruction}', prompt)
-            else:
-                raw_label = cached_output_label
-            raw_label_predictions.append(raw_label)
+            raw_label = self.read_from_merged_file(model_name_suffix, dataset['text'][index]) if self.enable_cache else None
+            if raw_label is None:
+                raw_label = predict_with_gemini(self.model, self.model_uri,f'{prompt_template.definition}\n{prompt_template.instruction}', prompt)
+
             predicted_label = self.output_label_converter.convert_label(raw_label)
+            raw_label_predictions.append(raw_label)
             label_predictions.append(predicted_label)
-            if cached_output_label is None:
+            if self.enable_cache:
                 self.append_into_merged_file(model_name_suffix, dataset, dataset['id'][index], predicted_label, raw_label)
 
         return super().predict_end(dataset, dataset_name, label_predictions, raw_label_predictions, model_name_suffix)
