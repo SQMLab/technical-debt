42670873a8f4d55132972d90bd47f3c971ca4e2d
shahidul <shahidul2k9@gmail.com>
2025-09-18 18:12:44 -0500

    fix output label conversion

--- satd-core/SatdToYesNoConverter.py
+++ satd-core/SatdToYesNoConverter.py
@@ -5,7 +5,7 @@ class SatdToYesNoConverter(OutputLabelConverter):
         self.output_label_converter = output_label_converter
         self.mapper = {'satd': 'yes', 'not-satd': 'no'}
     def convert_label(self, label):
-        predicted_output_label = self.output_label_converter.convert_label(label)
+        predicted_output_label = self.output_label_converter.convert_label(label).lower()
         if predicted_output_label in self.mapper:
             return self.mapper[predicted_output_label.lower()]
         else:
