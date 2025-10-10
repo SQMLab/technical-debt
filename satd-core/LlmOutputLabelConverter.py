a9ae0cb9ee74e5ef8dfc408ab9c11c8bf0c14106
Shahidul Islam <shahidul2k9@gmail.com>
2025-10-10 12:26:12 -0500

    Fix label parsing

--- satd-core/LlmOutputLabelConverter.py
+++ satd-core/LlmOutputLabelConverter.py
@@ -4,8 +4,8 @@ class LlmOutputLabelConverter(OutputLabelConverter):
         super().__init__(known_labels, unmatched_label)
     def convert_label(self, label):
         cleaned_label = label
-        for key in [':', '**', '.', ',', 'answer', 'label']:
-            cleaned_label = cleaned_label.replace(key, '')
+        for key in [':', '*', '.', ',', 'answer', 'label']:
+            cleaned_label = cleaned_label.replace(key, ' ')
         words = cleaned_label.lower().split()
         for word in reversed(words):
             if word in self.known_labels:
