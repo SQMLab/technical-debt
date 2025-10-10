311d209d0e9434f2fb1d8de983b799c61832094b
Shahidul Islam <shahidul2k9@gmail.com>
2025-10-10 12:54:12 -0500

    Add quote replacement

--- satd-core/LlmOutputLabelConverter.py
+++ satd-core/LlmOutputLabelConverter.py
@@ -1,10 +1,13 @@
 from OutputLabelConverter import OutputLabelConverter
+
+
 class LlmOutputLabelConverter(OutputLabelConverter):
     def __init__(self, known_labels: set[str], unmatched_label: str):
         super().__init__(known_labels, unmatched_label)
+
     def convert_label(self, label):
         cleaned_label = label
-        for key in [':', '*', '.', ',', 'answer', 'label']:
+        for key in ["assistantfinal", 'answer', 'label', ':', '*', '.', '"', ',']:
             cleaned_label = cleaned_label.replace(key, ' ')
         words = cleaned_label.lower().split()
         for word in reversed(words):
